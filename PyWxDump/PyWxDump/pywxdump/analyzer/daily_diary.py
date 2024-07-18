3cc4cdedea2523418f4322d654342e7eef8c64df
lamonkey <88therisingsun@gmail.com>
2024-07-18 10:35:28 -0400

    refactor and adding new function 1.added geenrate_diary function

--- PyWxDump/PyWxDump/pywxdump/analyzer/daily_diary.py
+++ PyWxDump/PyWxDump/pywxdump/analyzer/daily_diary.py
@@ -1,21 +1,18 @@
-from pywxdump.dbpreprocess.dbbase import DatabaseBase
-from pywxdump.dbpreprocess import wxid2userinfo
 import os
 from datetime import datetime, timedelta
+from functools import lru_cache
+from pywxdump.dbpreprocess.dbbase import DatabaseBase
+from pywxdump.dbpreprocess import wxid2userinfo
+from openai import OpenAI
 
-'''
-generate a diary
-'''
+def setup_openai_api(api_key):
+    return OpenAI(api_key=api_key)
 
-db_path = os.path.join(os.path.expanduser("~"),
-                       "Downloads",
-                       "pywxdumpv3027",
-                       "wxdump_tmp",
-                       "a38655162",
-                       "merge_all.db")
-db = DatabaseBase(db_path)
+def setup_database(db_path):
+    return DatabaseBase(db_path)
 
 
+@lru_cache(maxsize=128)
 def get_talker_name(db_path, wxid, is_sender):
     if is_sender:
         return 'me'
@@ -28,7 +25,7 @@ def get_talker_name(db_path, wxid, is_sender):
         return talker_name[wxid]['account']
 
 
-def generate_diary(date):
+def get_chats_for_diary(db, db_path, date):
     query_date = datetime.strptime(date, "%Y-%m-%d")
     two_day_ago = query_date - timedelta(days=2)
     query = "SELECT * FROM WL_MSG WHERE CreateTime >= ? AND CreateTime <= ? ORDER BY CreateTime DESC"
@@ -47,6 +44,29 @@ def generate_diary(date):
     return "\n".join(text_logs)
 
 
+def generate_diary(text_log, client):
+    system_prompt = """
+You are a editor at new york time, which given a task to create a diary based on \
+the chat log, the chat log most likely contain three consequtive days.\
+if the text_log is empty tell user there is no chat log for this day.\
+output the result in chinese.\
+"""
+   
+    response = client.chat.completions.create(
+        model="gpt-4-turbo",
+        messages=[
+            {'role':'system', "content": system_prompt},
+            {'role':'user', "content": text_log}
+        ]
+
+
+    )
+    return response.choices[0].message.content
+
+
 if __name__ == "__main__":
-    print(generate_diary("2024-07-01"))
+    db_path = 'c:/Users/jianl/Downloads/pywxdumpv3027/wxdump_tmp/a38655162/merge_all.db'
+    db = setup_database(db_path)
+    text_log = get_chats_for_diary(db, db_path, "2024-06-19")
+    print(generate_diary(text_log))
     # print(get_talker_name(db_path, "20461543530@chatroom", False))
